2ea3c5bdf22eacc232f5bd47808067cf31603b7f
ImElan <elansakthivel2000@gmail.com>
2020-07-30 21:12:30 +0530

    create responsive gpa cgpa card

--- Pages/view/[id].js
+++ Pages/view/[id].js
@@ -1,26 +1,30 @@
 import React from 'react';
+import GpaCgpaCard from '../../src/Components/UI/GpaCgpaCard';
+import { Container } from 'react-bootstrap';
 
 function ViewPage(props) {
       const { data } = props;
       console.log(data);
       return(
-            <h3>
-                  {data ? 'Data fetched from local storage':'No data stored in local storage'}
-            </h3>
+            <Container className='mt-4'>
+                  <GpaCgpaCard />
+            </Container>
       )
 }
 
 ViewPage.getInitialProps = async (props) => {
       const { id } = props.query;
-      /* 
-            1.return id for now.
-            2. then depending on id retrieve appropriate thing 
-                  ---> id = gpa (get gpa from local storage and return it as a prop)
-                  ---> id = cgpa (get cgpa from local storage and return it as a prop)
-      */ 
       const storedValue = window.localStorage.getItem(id);
       const data = storedValue ? JSON.parse(storedValue) : null;
       return { data };
 }
 
 export default ViewPage;
+
+// -> done
+/* 
+      1.return id for now.
+      2. then depending on id retrieve appropriate thing 
+            ---> id = gpa (get gpa from local storage and return it as a prop)
+            ---> id = cgpa (get cgpa from local storage and return it as a prop)
+*/ 
